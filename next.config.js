1e93f2f735e0ea9ccf1bd95bb9ebaf1df2c187a1
Barry Saik <barry@hellosoundry.com>
2024-10-09 15:13:01 -0700

    change connect-src to analytics insteadof cloud

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const contentSecurityPolicy = [
   `img-src * data:`,
   `script-src 'self' 'unsafe-eval' 'unsafe-inline'`,
   `style-src 'self' 'unsafe-inline'`,
-  `connect-src 'self' api.umami.is cloud.umami.is`,
+  `connect-src 'self' analytics.soundryhealth.com`,
   `frame-ancestors 'self' ${frameAncestors}`,
 ];
 
